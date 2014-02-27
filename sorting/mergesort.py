caf23224f326061b52da72bad701ef6084ed72e2
namelessnerd <karthik.gomadam@gmail.com>
2014-02-27 15:49:35 -0800

    adding quicksort; enabling main on merge sort. eventually there will be one script that accepts teh sorting algo and calls the test mod. for now each sorting algo has to be run by itself

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,14 +1,20 @@
 
 '''
-This is merge sort implementation. Merge sort takes a list and splits the list into two, left and right. These are again recursively split, until we have lists with 1 element each.
-These lists are merged by the merge_left_right method. The main intuition behind the merge is that given two sorted lists, one can create a merged sorted list in o(n).
-The merge_left_right method checks if the head of left list is less than or equal to (ascending order sort) the head of the right list. Since these two are sorted lists, 
+This is merge sort implementation. Merge sort takes a list and splits the list into two, left and right.
+ These are again recursively split, until we have lists with 1 element each.
+These lists are merged by the merge_left_right method. The main intuition behind the merge is that given 
+two sorted lists, one can create a merged sorted list in o(n).
+The merge_left_right method checks if the head of left list is less than or equal to (ascending order sort) 
+the head of the right list. Since these two are sorted lists, 
 if the head of the left list is LEQ head of right list, all of the right list is GEQ to this element. 
 
-The time complexity is nlog(n). Merge sort has a space complexity of o(n) + o(log n) (to keep stack frames in recurson), yielding o(n) as it does not do in place sort.
+The time complexity is nlog(n). Merge sort has a space complexity of o(n) + o(log n) (to keep stack frames 
+in recurson), yielding o(n) as it does not do in place sort.
 
-The mergetuple_left_right method takes a list of tuples and a position argument by which this must be sorted. For example, to sort a list of strings by their length, 
-one can first create a tuple that of the format (string length, position in list). The merge_sort method then sorts the strings by length. Since we are saving the position 
+The mergetuple_left_right method takes a list of tuples and a position argument by which this must be sorted. 
+For example, to sort a list of strings by their length, 
+one can first create a tuple that of the format (string length, position in list). 
+The merge_sort method then sorts the strings by length. Since we are saving the position 
 in the original list, one could create a set of strings sorted by their length.
 
 The code for this would be:
@@ -84,10 +90,17 @@ def test_string_list():
 	print 'This method tests merge sort with a list of strings that need to be sorted by their string length'
 	list_of_strings= ['cat', 'cab', 'bat', 'bark', 'bard', 'cow', 'as']
 	print 'List before sorting: {0}'.format(list_of_strings)
-	sorted_string=[list_of_strings[sorted_tuple[1]] for sorted_tuple in merge_sort([(len(v),k) for k,v in enumerate(list_of_strings)], type='tuple')]
+	sorted_string=[list_of_strings[sorted_tuple[1]] for sorted_tuple in merge_sort
+		([(len(v),k) for k,v in enumerate(list_of_strings)], type='tuple')]
 
 def test_short_list():
 	print 'This method tests merge sort with a list of 100 integers'
 	list_of_int= [random.randint(0,100) for i in range(100)]
 	print 'List before sorting: {0}'.format(list_of_int)
 	print 'List after sorting: {0}'.format(merge_sort(list_of_int))
+
+def main():
+	test_short_list()
+
+if __name__ == '__main__':
+	main()
---
+++ sorting/quicksort.py
@@ -0,0 +1,70 @@
+'''
+Quicksort is a divide and conquer algorithm. In Quicksort, a pivot element is 
+chosen and the list is divided into two halves: to the left of the pivot with all values
+smaller than the pivot and to the right of the pivot, with all other values, including 
+that of the pivot (if it appears multiple times). The left is sorted and the right list 
+is sorted. 
+
+Pivot and Partition(create partition method): 
+This is the critical method in quicksort. To create the partition, we identify the pivot. 
+Starting from both the ends, traverse the list. From the left, skip elements that are 
+less than the pivot and from the right, skip elements that are greater than the pivot. 
+If we have an element in left that is greater than the pivot AND an element that is either 
+equal or less than the pivot to the right, swap them. 
+Terminate once the left side marker crosses the right side marker and return the left side 
+market.
+
+Call quicksort on the sublist terminating at the left side marker and on rest of the list 
+(Starting one position to the right of the marker)
+
+Like all divide and conquer algorithms, the time complexity of Quicksort is calculated by 
+a recurrence relation. The relation for Quicksort is: T(n)= 2T(n/2) + O(n), ie. the time 
+complexity of the sort is the sum of the time complexity for sorting each of the halves, plus
+the time complexity to find the pivot. 
+
+Pivot selection affects the performance of the algorithm. In worst case, Quicksort can be O(n2),
+when repeatedly extreme pivots are chosen (the largest or smallest element). The best approach 
+to select a pivot is to select the median element (one that has half of the list less than itself).
+The common practise is to just pick the middle element of the list. 
+
+Even though other algorithms have better worst case, with same average case, Quicksort has been 
+proven to be efficent in practice. 
+The Quicksort algorithm below derives from CLR approach. 
+'''
+
+def create_partition(list, low, high):
+	pivot= list[(low+ high)/2]
+	while low< high:
+		while list[low]<pivot:
+			low+=1
+		while list[high]>pivot:
+			high-=1
+		if low <= high:
+			tmp= list[low]
+			list[low]= list[high]
+			list[high]= tmp
+			low+=1
+			high-=1
+	return low
+
+
+def quicksort(list, low, high):
+	if low< high:
+		idx= create_partition(list, low, high)
+		print list
+		quicksort(list, low, idx-1)
+		print 10*"--"
+		print idx
+		print high
+		quicksort(list,idx, high)
+
+def test():
+	list= [1, 12, 5, 26, 7, 14, 3, 7, 2,]
+	quicksort(list, 0, 8)
+	print list
+
+def main():
+	test()
+
+if __name__ == '__main__':
+	main()
